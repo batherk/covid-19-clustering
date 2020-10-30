f8d9d10ec3f0bd0d2367df28548e6e6f5e2a1506
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2020-10-30 16:42:48 +0100

    Fix too-long-line error

--- Source/Data/owid_data_extractor.py
+++ Source/Data/owid_data_extractor.py
@@ -1,7 +1,32 @@
 import numpy as np
 import csv
 
-ATTRIBUTES = ['iso_code', 'continent', 'location', 'total_cases_per_million', 'total_deaths_per_million', 'tests_per_case', 'positive_rate', 'tests_units', 'stringency_index', 'population', 'population_density', 'median_age', 'aged_65_older', 'aged_70_older', 'gdp_per_capita', 'extreme_poverty', 'cardiovasc_death_rate', 'diabetes_prevalence', 'female_smokers', 'male_smokers', 'handwashing_facilities', 'hospital_beds_per_thousand', 'life_expectancy', 'human_development_index']
+ATTRIBUTES = [
+    'iso_code',
+    'continent',
+    'location',
+    'total_cases_per_million',
+    'total_deaths_per_million',
+    'tests_per_case',
+    'positive_rate',
+    'tests_units',
+    'stringency_index',
+    'population',
+    'population_density',
+    'median_age',
+    'aged_65_older',
+    'aged_70_older',
+    'gdp_per_capita',
+    'extreme_poverty',
+    'cardiovasc_death_rate',
+    'diabetes_prevalence',
+    'female_smokers',
+    'male_smokers',
+    'handwashing_facilities',
+    'hospital_beds_per_thousand',
+    'life_expectancy',
+    'human_development_index'
+]
 
 
 def extract_data_from_owid_covid_data():
