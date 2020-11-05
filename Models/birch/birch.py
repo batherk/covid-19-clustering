70e71efbcd48cd9d240e159b2fcec780667938c0
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2020-11-05 19:56:18 +0100

    Initialize birch and minor cleanup

--- Models/birch/birch.py
+++ Models/birch/birch.py
@@ -1,22 +1,17 @@
 import pandas as pd
 import numpy as np
-from sklearn.cluster import SpectralClustering
+from sklearn.cluster import Birch
 
 # Read the processed data from our EDA
 data = pd.read_csv('../../Data/Processed/processed.csv')
 
-# Initialize Spectral Clusering model
-sc = SpectralClustering(
-    n_clusters=20,
-    affinity='nearest_neighbors',
-    n_init=100,
-    assign_labels='discretize'
-)
+# Initialize Birch model
+birch = Birch(n_clusters=None)
 
 # Fit data to model and predict labels for data
-sc.fit_predict(data)
+birch.fit_predict(data)
 
 # Get the predicted labels (clusters) for each row of our dataset
-y_pred = sc.labels_.astype(np.int)
+y_pred = birch.labels_.astype(np.int)
 
 print(y_pred)
