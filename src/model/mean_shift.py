cd13c386c0d8e88ae3ac31430c639db31b189fba
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2020-11-16 12:21:16 +0100

    Add mean shift

---
+++ src/model/mean_shift.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn.cluster import MeanShift
+import joblib
+from datetime import datetime
+
+# Hyperparameters
+n_clusters = 20
+n_init = 100
+affinity = 'rbf'
+assign_labels = 'discretize'
+
+# Initialize mean shift
+mean_shift = MeanShift()
+
+model = dict({
+    'model': mean_shift,
+    'metadata': {
+        'name': 'Mean Shift',
+        'abbreviation': 'MS',
+        'datetime': str(datetime.now()),
+        'hyperparameters': {
+        },
+        'metrics': {
+            'silhouette coefficient': 0
+        }
+    }
+})
+
+# Read the processed data from the EDA
+X = pd.read_csv('data/processed/processed.csv')
+
+# Train model
+mean_shift.fit_predict(X)
+y_pred = mean_shift.labels_
+print(y_pred)
+
+# Persist model and metadata
+joblib_filename = 'models/mean_shift.joblib'
+joblib.dump(model, joblib_filename)
