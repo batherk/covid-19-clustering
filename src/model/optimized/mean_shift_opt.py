c762276f31be48b8de0857ed4285c6d8eea979e4
Bjørn Are Therkelsen <ba.therk@outlook.com>
2020-11-25 00:11:13 +0100

    Add optimized model of Mean Shift

---
+++ src/model/optimized/mean_shift_opt.py
@@ -0,0 +1,40 @@
+# type: ignore
+import pathsetup  # noqa
+import joblib
+import pandas as pd
+from datetime import datetime
+from src.utils import save_clusters_as_csv, save_clustering_metrics_as_csv
+from sklearn.cluster import MeanShift
+
+
+# Hyperparameters
+bandwidth = 0.60
+
+# Initialize mean shift
+mean_shift = MeanShift(bandwidth=bandwidth)
+
+model = dict({
+    'model': mean_shift,
+    'metadata': {
+        'name': 'MeanShift',
+        'abbreviation': 'MS',
+        'datetime': str(datetime.now()),
+        'hyperparameters': {
+            'bandwidth': bandwidth
+        },
+    }
+})
+
+# Read the processed data from the EDA
+X = pd.read_csv('data/processed/processed.csv')
+
+# Cluster and save results
+mean_shift.fit_predict(X)
+labels = mean_shift.labels_
+save_path = 'results/mean_shift/optimized'
+save_clusters_as_csv(labels, save_path)
+save_clustering_metrics_as_csv(X, labels, save_path)
+
+# Persist model and metadata
+joblib_filename = 'models/mean_shift_opt.joblib'
+joblib.dump(model, joblib_filename)
