f693bf1579d14b461c0c902d419b6efa4a13e80f
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2020-11-25 08:31:11 +0100

    Update map plot

--- src/visualization/map_plot.py
+++ src/visualization/map_plot.py
@@ -3,28 +3,28 @@ import plotly.express as px
 
 data = pd.read_csv('data/clean/clean.csv')
 methods = {
-    'agglomerative_clustering_opt': pd.read_csv(
+    'agglomerative_clustering': pd.read_csv(
         'results/agglomerative_clustering/optimized/clusters.csv'),
-    'agglomerative_clustering_opt_weighted': pd.read_csv(
+    'agglomerative_clustering_weighted': pd.read_csv(
         'results/agglomerative_clustering/optimized_weighted/clusters.csv'),
-    'birch_opt': pd.read_csv(
-        'results/birch/optimized/clusters.csv'),
-    'birch_opt_weighted': pd.read_csv(
-        'results/birch/optimized_weighted/clusters.csv')
+    'mean_shift': pd.read_csv(
+        'results/mean_shift/optimized/clusters.csv'),
+    'mean_shift_weighted': pd.read_csv(
+        'results/mean_shift/optimized_weighted/clusters.csv')
 }
-methods['diff_opt'] = pd.DataFrame({
-    'cluster': (methods['agglomerative_clustering_opt'] == methods['birch_opt']).all(axis=1)
+methods['diff'] = pd.DataFrame({
+    'cluster': (methods['agglomerative_clustering'] == methods['mean_shift']).all(axis=1)
 })
 methods['weighted_diff_ac'] = pd.DataFrame({
-    'cluster': (methods['agglomerative_clustering_opt'] == methods['agglomerative_clustering_opt_weighted']).all(axis=1)
+    'cluster': (methods['agglomerative_clustering'] == methods['agglomerative_clustering_weighted']).all(axis=1)
 })
-methods['weighted_diff_birch'] = pd.DataFrame({
-    'cluster': (methods['birch_opt'] == methods['birch_opt_weighted']).all(axis=1)
+methods['weighted_diff_mean_shift'] = pd.DataFrame({
+    'cluster': (methods['mean_shift'] == methods['mean_shift_weighted']).all(axis=1)
 })
 
 for method, clustering in methods.items():
     fig = px.choropleth(
-        pd.concat([data, clustering], axis=1).applymap(str),
+        pd.concat([data, clustering], axis=1).applymap(str).sort_values(by=['cluster']),
         locations='iso_code',
         color='cluster',
         title=method,
